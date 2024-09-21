2e72e185c312ddf07e6b55b647c13ae67b4ef68d
chenhaoen <1600045793@qq.com>
2024-09-21 14:37:26 +0800

    删除不需要的脚本

--- platform/linux/compile_shaders.sh
+++
@@ -1,2 +0,0 @@
-/home/che/Downloads/install/bin/glslc ../../examples/triangle/shaders/shader.vert -o ../../examples/triangle/shaders/vert.spv
-/home/che/Downloads/install/bin/glslc ../../examples/triangle/shaders/shader.frag -o ../../examples/triangle/shaders/frag.spv
--- platform/windows/compile.bat
+++
@@ -1,2 +0,0 @@
-E:\VulkanSDK\Bin\glslc.exe ../../examples/triangle/shaders/shader.vert -o ../../examples/triangle/shaders/vert.spv
-E:\VulkanSDK\Bin\glslc.exe ../../examples/triangle/shaders/shader.frag -o ../../examples/triangle/shaders/frag.spv
