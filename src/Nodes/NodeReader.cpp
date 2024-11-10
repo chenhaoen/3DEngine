76f0bd4b4793b958791340809bfd68fa37b03470
chenhaoen <1600045793@qq.com>
2024-11-10 11:57:38 +0800

    修复assimp编译问题

--- src/Nodes/NodeReader.cpp
+++ src/Nodes/NodeReader.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include "Nodes/NodeReader.h"
 
 Node *NodeReader::read(const std::string_view &file)
 {
+    Assimp::Importer import;
+    const aiScene *scene = import.ReadFile(file.data(), aiProcess_Triangulate | aiProcess_FlipUVs); 
+
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    {
+        std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+        return nullptr;
+    }
+
     return nullptr;
 }
